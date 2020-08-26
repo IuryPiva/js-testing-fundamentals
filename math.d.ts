02ea146130d99b5b007cce33f339f6ca9a23855e
Iury Piva <iurypiva@gmail.com>
2020-08-26 19:55:12 -0300

    simple expect

---
+++ math.d.ts
@@ -0,0 +1,4 @@
+export function sum(a: number, b: number): number
+export function subtract(a: number, b: number): number
+export function sumAsync(a: number, b: number): Promise<number>
+export function subtractAsync(a: number, b: number): Promise<number>
--- ts-lessons/simple.ts
+++ ts-lessons/simple.ts
@@ -1,16 +1,18 @@
-const sum = (a: number, b: number) => a + b;
-const subtract = (a: number, b: number) => a - b;
+import { sum, subtract } from "../math";
 
 let result = sum(3, 7);
 let expected = 10;
-
-if (result !== expected)
-  throw new Error(`${result} is not equal to ${expected}`);
+expect(result).toBe(expected);
 
 result = subtract(7, 3);
 expected = 4;
+expect(result).toBe(expected);
 
-if (result !== expected)
-  throw new Error(`${result} is not equal to ${expected}`);
-
-console.log('Pass all tests');
+function expect<T>(actual: T) {
+  return {
+    toBe(expected: T) {
+      if (actual !== expected)
+        throw new Error(`${actual} is not equal to ${expected}`);
+    },
+  };
+}
